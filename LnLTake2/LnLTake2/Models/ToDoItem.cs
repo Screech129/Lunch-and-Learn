083d48f6451beecda24eda88e8ee14fe203ba1a8
David Silver <david.silver@sparkhound.com>
2016-09-07 08:30:22 -0500

    Test commit

---
+++ LnLTake2/LnLTake2/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using AppServiceHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LnLTake2.Models
+{
+    public class ToDoItem:EntityData
+    {
+        public string Name { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
