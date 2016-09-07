944e2961ce9c9a768ddaee7f4200e6841e9c595f
David Silver <david.silver@sparkhound.com>
2016-09-07 09:02:36 -0500

    Got webservices backend working and todo list setup

---
+++ LnLTest1/LnLTest1/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,113 @@
+﻿using LnLTest1.Data;
+using LnLTest1.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LnLTest1.ViewModels
+{
+    public class ToDoListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        DbHelper DbHelper = new DbHelper();
+        public ToDoListViewModel ()
+        {
+            AddToDoItem = new Command(async () => await AddItem());
+        }
+
+
+        public async Task FillToDoItems ()
+        {
+            //await DbHelper.TestData();
+            var toDoList = await DbHelper.Get();
+            ToDoItems = new ObservableCollection<ToDoItem>(toDoList.Where(x => x.Done == false));
+        }
+
+
+        public async Task AddItem ()
+        {
+            var item = new ToDoItem
+            {
+                Done = false,
+                Name = NewItem
+            };
+            await DbHelper.Insert(item);
+            await FillToDoItems();
+        }
+
+        public async Task CompleteItem ()
+        {
+
+            await DbHelper.Update(selectedItem);
+            await FillToDoItems();
+        }
+
+        private bool done;
+        public bool Done
+        {
+            get
+            {
+                return done;
+            }
+            set
+            {
+                done = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Done"));
+            }
+
+        }
+
+        private string newItem;
+        public string NewItem
+        {
+            get
+            {
+                return newItem;
+            }
+            set
+            {
+                newItem = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NewItem"));
+
+            }
+
+        }
+
+        private ObservableCollection<ToDoItem> toDoItems;
+        public ObservableCollection<ToDoItem> ToDoItems
+        {
+            get
+            {
+                return toDoItems;
+            }
+            set
+            {
+                toDoItems = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ToDoItems"));
+            }
+        }
+
+        private ToDoItem selectedItem;
+
+        public ToDoItem SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                selectedItem = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedItem"));
+            }
+        }
+
+        public ICommand AddToDoItem { get; protected set; }
+    }
+}
---
+++ LnLTest1/LnLTest1/Views/ToDoView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LnLTest1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace LnLTest1.Views
+{
+    public partial class ToDoView : ContentPage
+    {
+        public ToDoView ()
+        {
+            InitializeComponent();
+
+            ToDoList.ItemSelected += async (s, e) => await CompleteItem();       
+        }
+
+        protected async override void OnAppearing ()
+        {
+            base.OnAppearing();
+            var vm = BindingContext as ToDoListViewModel;
+            await vm.FillToDoItems();
+        }
+
+        
+        
+        private async Task CompleteItem()
+        {
+            var vm = BindingContext as ToDoListViewModel;
+            await vm.CompleteItem();
+        }
+    }
+}
