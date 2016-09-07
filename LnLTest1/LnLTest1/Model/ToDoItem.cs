4e5d89ed81a848e24787209d8652adb73f72770e
David Silver <david.silver@sparkhound.com>
2016-09-07 08:39:49 -0500

    Got the todo list working and attached to mobile services

---
+++ LnLTest1/LnLTest1/Model/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LnLTest1.Data
+{
+    public class ToDoItem: RealmObject
+    {
+        [Indexed]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+}
